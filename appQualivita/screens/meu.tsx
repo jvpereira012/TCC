5b0bc761fdf09bc060d40a778608c7f4f74c4998
João Pereira <joaovitordejesus855@gmail.com>
2024-07-17 16:37:40 -0300

    Alteração de arquivos na pasta assests e criação da tela de splash

---
+++ appQualivita/screens/meu.tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text} from 'react-native';
+
+export const meuComponente = () =>{
+   <SafeAreaView>
+    <Text>Loiras</Text>
+    <Text>Morenas</Text>
+    <Text>Ruivas</Text>
+   </SafeAreaView>
+}
