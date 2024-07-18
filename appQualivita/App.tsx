0219931119c2c578639b3c033d4baa3e138d27d7
João Pereira <joaovitordejesus855@gmail.com>
2024-07-18 17:37:21 -0300

    instalação dos módulos react navigation no projeto

--- appQualivita/App.tsx
+++ appQualivita/App.tsx
@@ -1,23 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Marchaaaa</Text>
 
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
-export default App;
