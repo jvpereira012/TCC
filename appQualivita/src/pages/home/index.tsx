4a49a2a5949ffb3f4da84d183289beae3b261d67
João Pereira <joaovitordejesus855@gmail.com>
2024-09-02 19:11:26 -0300

    Inserção do mapa na tela com marcador personalizado e manipulação da localização para testes

--- appQualivita/src/pages/home/index.tsx
+++ appQualivita/src/pages/home/index.tsx
@@ -1,10 +1,60 @@
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
-import React from 'react';
+import { StyleSheet, SafeAreaView, Image } from 'react-native';
+import {getCurrentPositionAsync, requestForegroundPermissionsAsync, LocationObject, watchPositionAsync, LocationAccuracy} from 'expo-location';
+import MapView, {Marker} from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [localizacao, setLocalizacao] = useState<LocationObject | null>(null);
+  async function requestLocationPermissions(){
+
+    const {granted} = await requestForegroundPermissionsAsync();
+    if(granted){
+       const currentposition =await getCurrentPositionAsync();
+       setLocalizacao(currentposition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1300,
+      distanceInterval: 1
+    }, (resposta) => {
+      setLocalizacao(resposta);
+    })
+  }, []);
+
     return (
       <SafeAreaView style={styles.container}>
-  
+       { 
+        localizacao &&
+          <MapView 
+            style={styles.map}
+            initialRegion={{
+              latitude: localizacao.coords.latitude,
+              longitude: localizacao.coords.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }}
+            >
+              <Marker
+                coordinate={{
+                  latitude: localizacao.coords.latitude,
+                  longitude: localizacao.coords.longitude,
+                }}
+              >              
+                <SafeAreaView style={styles.markerContainer}>
+                  <Image
+                    source={require('../../../assets/iconeseimagens_app/icon1.png')}
+                  />
+                </SafeAreaView>
+              </Marker>
+          </MapView>
+        }
       </SafeAreaView>
     );
 }
@@ -12,6 +62,20 @@ export default function Home() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    flexDirection:'column'
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map:{
+    flex: 1,
+    width: '100%',
+  },
+  markerContainer: {
+    height: 34,
+    width: 45,
+    flexDirection: 'column',
+    overflow: 'hidden',
+    alignItems: 'center',
+    borderRadius: 0
   }
 })
